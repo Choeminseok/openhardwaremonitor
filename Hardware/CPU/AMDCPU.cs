781b10ab5e01060577f106c743c033dd153ca86f
Choeminseok <44373916+Choeminseok@users.noreply.github.com>
2025-03-09 20:59:22 +0900

    Update AMDCPU.cs

--- Hardware/CPU/AMDCPU.cs
+++ Hardware/CPU/AMDCPU.cs
@@ -1,11 +1,11 @@
-﻿/*
- 
+/*
+  
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
+  
   Copyright (C) 2009-2010 Michael Möller <mmoeller@openhardwaremonitor.org>
-	
+ 	
 */
 
 namespace OpenHardwareMonitor.Hardware.CPU {
@@ -38,5 +38,20 @@ namespace OpenHardwareMonitor.Hardware.CPU {
       return address;
     }
 
+    // 추가된 코드: AMD Ryzen 9 9600X의 온도를 읽어오는 메서드
+    protected float ReadTemperature() {
+      // 예시 코드, 실제 온도 읽기 로직을 구현해야 함
+      uint address = GetPciAddress(0, 0x1437); // AMD Family 19h Model 21h (Zen 3) CPU
+      if (address == Ring0.InvalidPciAddress)
+        return float.NaN;
+
+      uint value;
+      if (!Ring0.ReadPciConfig(address, 0xA4, out value)) // Temperature Register
+        return float.NaN;
+
+      // 온도를 계산하는 로직
+      float temperature = ((value >> 21) & 0x7FF) / 8.0f;
+      return temperature;
+    }
   }
 }
